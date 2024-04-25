9cf46acba286074d6f66472d929f98cbba3b260e
rambus2006 <w2227@e-mirim.hs.kr>
2024-04-25 12:16:26 +0900

    UIUX 수업 정리

---
+++ 08_AJAX/express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin:"http://127.0.0.1:5500",
+    credentials:true,
+}))
+app.get("/",(req,res)=>{
+    
+    const jsonData = new Array();
+    jsonData.push({"name" : "minseo"});
+    jsonData.push({"name" : "joy"});
+    jsonData.push({"name":"Try"});
+    res.send(jsonData);
+});
+app.post("/",(req,res)=>{
+    const jsonData = new Array();
+    jsonData.push({"name" : "1minseo"});
+    jsonData.push({"name" : "1joy"});
+    jsonData.push({"name":"1Try"});
+    res.send(jsonData);
+});
+app.listen(3005);
